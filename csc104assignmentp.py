275466944c07896522828ffe3d508532ceab8480
yusuf772 <81990008+yusuf772@users.noreply.github.com>
2021-04-05 20:28:20 +0100

    Add files via upload

---
+++ csc104assignmentp.py
@@ -0,0 +1,29 @@
+'''Write a short python code to display the name of your name and department'''
+name = "Yusuf Abdullahi Abubakar"
+department = "computer science"
+print(name,"\n",department,)
+'''Assign the string "Boo!" to the variable scare and print the variable'''
+scare = "Boo!"
+print (scare)
+'''Add two variables and assign the result to a third variable. print all'''
+first_variable = 8
+second_variable = 5
+third_variable = (first_variable + second_variable)
+print(third_variable)
+'''Divide 12 by the decimal .5 and assign the result to the variable outcome and print the variable'''
+division = 12/.5
+outcome = (division)
+print(outcome)
+'''Assign the sum of two numbers to a variable and print the variable'''
+sum_of_two_numbers = 7 + 9
+print(sum_of_two_numbers)
+'''Write the code that finds the remainder when 8 is divided by 3'''
+a = 8
+b = 3
+modulus = a%b
+print (modulus)
+'''Rewrite the following statement to force this order: First, multiply 2 by 4. Then add 4 and 2. Then multiply the first result by the second result. Print the result.'''
+order1 = 2 * 4
+order2 = 4 + 2
+order3 = order1 * order2
+print(order3)
